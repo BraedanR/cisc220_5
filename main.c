d615e7d62f787eff6680c9f56ff3ddedb23f90be
braedanR <braedantye@msn.com>
2016-12-01 17:13:20 -0500

    new main

--- main.c
+++ main.c
@@ -1,18 +1,23 @@
 //Braedan Robinson 10188414
 //Luisa Stocco
+#include <stdio.h>
+#include "udll.h"
 
-union linkedList {
+union Data {
 
-int i;
-int* iPtr;
-float f;
-float* fPtr;
-char c;
-char* cPtr;
+  int i;
+  int* iPtr;
+  float f;
+  float* fPtr;
+  char c[20];
+  char* cPtr;
 };
 
-
 int main() {
-
+  int index = 0;
+  union Data data;
+  data.i = 10;
+  insert(index, data, 0);
+  
 return 0;
 }
