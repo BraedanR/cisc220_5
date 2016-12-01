f00493e2bc9edcf72980d0110e1e04dedb304a01
luisastocco <luisa_stoccoaimoli@hotmail.com>
2016-12-01 14:53:55 -0500

    2

--- luisa.c
+++ luisa.c
@@ -5,10 +5,27 @@
 #include <stdlib.h>
 #include <assert.h>
 
+//struct Data {
+//	int data;
+//	struct Node* next;
+//};
+//struct Node *head;
+//head->data = Data.len;
+
+//void insert(int data, int n) {
+//	Node* temp1 = new Node();
+//	temp1->data = data;
+//	temp->next = NULL;
+//	if (n==1) {
+//		temp->next = head;
+//		head = temp1;
+//		return;
+
+//returns node data at given index
 union Data get(int index) {
 
 int count=0; //initialize index
-struct node* current = data.len;
+struct node* current = head;
 int length = LinkedListLength(current);
 
 if (index <= 0 || index > length) {
@@ -17,17 +34,16 @@ if (index <= 0 || index > length) {
 } else {
 	while (current != NULL) { //loop through link list
 		if (count == index)
-			return (current->data);//or data.current //minimum number of iterations
+			return current->data;//or current.data //minimum number of iterations
 		count++;
-		current = current->next; //next item
+		current = current->next; //next node if not correct index
 }
 }
-
 //dont forget to free the struct on maiin
 
+//returns the length of the linked list
 int length() {
-
-return data.len; 
+	return head->data; //where the length is stored 
 //update len insert;
 //update len remove
 }
