2da7df25336127d6832afaca65236e1a87a82ea3
luisastocco <luisa_stoccoaimoli@hotmail.com>
2016-12-01 22:54:18 -0500

    Update main.c

--- main.c
+++ main.c
@@ -20,6 +20,13 @@ struct Node {
   union Data data;
 };
 
+struct Node *last = NULL;
+struct Node *head; //global
+struct Node *afterhead; //global
+head->data = NULL
+head->next = afterhead;
+head->type = 0;
+
 int main() {
   afterhead = NULL; //empty list
   union Data data;
